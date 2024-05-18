3e4c98fd7e313d1531db1ec7d3db6da85e32d05c
Vinicius01P <vinicius.paulino@alunos.unis.edu.br>
2024-05-18 17:30:56 -0300

    Guitarra classe exercício

---
+++ Guitarra.java
@@ -0,0 +1,67 @@
+package unidade_1;
+
+public class Guitarra {
+
+	private String numeroSerie, fabricante, modelo, tipo, madeira;
+	private double preco;
+
+
+	public Guitarra(
+				String numeroSerie, String fabricante,
+				String modelo, String tipo,
+				String madeira, double preco) {
+			this.numeroSerie = numeroSerie;
+			this.fabricante = fabricante;
+			this.modelo = modelo;
+			this.tipo = tipo;
+			this.madeira = madeira;
+			this.preco = preco;
+	}
+	public String getNumeroSerie() {
+		return numeroSerie;
+	}
+	public void setNumeroSerie(String numeroSerie) {
+		this.numeroSerie = numeroSerie;
+	}
+	public String getFabricante() {
+		return fabricante;
+	}
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getMadeira() {
+		return madeira;
+	}
+	public void setMadeira(String madeira) {
+		this.madeira = madeira;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+public static void main(String[] args) {
+	Guitarra minhaGuitarra = new Guitarra("02055304", "squier", "stratocaster", "elétrica", "mogno", 1500);
+
+	System.out.printIn(minhaGuitarra.getNumeroSerie());
+	System.out.printIn(minhaGuitarra.getMadeira());
+	System.out.printIn(minhaGuitarra.getPreco());
+	System.out.printIn(minhaGuitarra.getTipo());
+
+	}
+
+}
